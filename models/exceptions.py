f3fbe0c955dee8c117a072d741a6dbb313e8767a
Genesis Gonzalez <genemar37@gmail.com>
2024-11-12 17:09:18 -0500

    Delete old exceptions file from models folder

--- models/exceptions.py
+++
@@ -1,8 +0,0 @@
-class ArtistNotFoundError(Exception):
-# Custom exception when an artist is not found
-	pass
-
-
-class SongNotFoundError(Exception):
-# Custom exception when a song is not found
-	pass
